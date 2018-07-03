62180f8446fd0ba097f1b0c0be40240fb73c86f7
tseng duke <bearbearbear10675@gmail.com>
2018-07-03 16:56:32 +0000

    hw4 2

--- HW4/duke4.go
+++ HW4/duke4.go
@@ -31,53 +31,52 @@ func main() {
 	//伐廠 每秒產1頓紙漿
 	wooderPower := wooderrrrr{
 		produceWooderKg: 1, //每秒產1頓紙漿
-		color:           31,
+		color:           33,
 	}
 
 	//紙廠1 每秒處理 0.5頓，製成5000張
 	paperPower1 := paperrrr{
 		useWooderKg:  0.5,
 		producePaper: 5000,
-		color:        32,
+		color:        20,
 	}
 	//紙廠2 每秒處理 0.3頓，製成3000張
 	paperPower2 := paperrrr{
 		useWooderKg:  0.3,
 		producePaper: 3000,
-		color:        33,
+		color:        20,
 	}
 
 	//印刷廠 每秒可印6000張紙
 	printPower := printerrrrr{
 		printNum: 6000,
-		color:    33,
+		color:    34,
 	}
 
 	//建立WaitGroup
 	var wg sync.WaitGroup
 	//建立互斥鎖
-	var mutex sync.Mutex
+	// var mutex sync.Mutex
 
 	allWood := float64(0) //目前有多少木頭
 	var allPaper = 0      //目前有多少紙
 	var allPrint = 0      //目前印了多少紙
 	//var maxPrint = 60000 //總共要印製60000張紙
 
-	wg.Add(4)
-
-	go wooderFunc(wooderPower, &allWood, &allPaper, &allPrint, &wg)
-	go paperFunc1(paperPower1, &allWood, &allPaper, &allPrint, &wg, mutex)
-	go paperFunc2(paperPower2, &allWood, &allPaper, &allPrint, &wg, mutex)
-	go printerFunc(printPower, &allPaper, &allPrint, &wg)
+	wg.Add(2)
+	go wooderFunc(wooderPower, &allWood, &allPrint)
+	go paperFunc(paperPower1, &allWood, &allPaper, &allPrint, &wg, 1)
+	go paperFunc(paperPower2, &allWood, &allPaper, &allPrint, &wg, 2)
+	go printerFunc(printPower, &allPaper, &allPrint)
 
 	//等待所有工作完成
 	wg.Wait()
 
 }
 
-func wooderFunc(w wooderrrrr, allWood *float64, allPaper *int, allPrint *int, wg *sync.WaitGroup) {
+func wooderFunc(w wooderrrrr, allWood *float64, allPrint *int) {
 
-	//印好60000紙之前都給我我好好伐木
+	//印好60000紙之前都給我好好去伐木木木木木
 	for i := *allPrint; i < 60000; i = *allPrint {
 		//工作前的數量
 		beforeWoodNum := *allWood
@@ -85,36 +84,30 @@ func wooderFunc(w wooderrrrr, allWood *float64, allPaper *int, allPrint *int, wg
 		//木總產量++
 		*allWood = *allWood + w.produceWooderKg
 
-		fmt.Printf("***伐木場:目前有%v噸木頭，累計生產了%v噸木頭\n", beforeWoodNum, *allWood)
+		fmt.Printf("%c[0;40;%dm伐木場:目前有%v噸木頭，累計生產了%v噸木頭\n%c[0m", 0x1B, w.color, beforeWoodNum, *allWood, 0x1B)
 
+		//休息
 		time_s := time.Duration(1) * time.Second
 		time.Sleep(time_s)
 	}
-
-	//工作完成 回報WaitGroup -1
-	wg.Done()
 }
 
-func paperFunc1(p paperrrr, allWood *float64, allPaper *int, allPrint *int, wg *sync.WaitGroup, mutex sync.Mutex) {
+func paperFunc(p paperrrr, allWood *float64, allPaper *int, allPrint *int, wg *sync.WaitGroup, who int) {
 
-	//印好60000紙之前都給我好好造紙
+	//印好60000紙之前都給我好好去造紙紙紙紙紙紙紙
 	for i := *allPrint; i < 60000; i = *allPrint {
-		//工作前的數量
+		//工作前的木頭數量
 		beforeWoodNum := *allWood
-		//鎖
-		mutex.Lock()
 
-		//木頭有超過可拿取量 就去生產紙
+		//若木頭有超過可拿取的量 就去生產紙
 		if *allWood >= p.useWooderKg {
 			*allPaper = *allPaper + p.producePaper //紙+
 			*allWood = *allWood - p.useWooderKg    //木頭-
-			fmt.Printf(">>>紙廠1:目前有%v噸木頭用，累計產了%v張紙，剩下%v噸木頭\n", beforeWoodNum, *allPaper, *allWood)
+			fmt.Printf("%c[0;40;%dm紙廠%v:目前有%v噸木頭用，累計產了%v張紙，剩下%v噸木頭\n%c[0m", 0x1B, p.color, who, beforeWoodNum, *allPaper, *allWood, 0x1B)
 		} else {
-			fmt.Printf(">>>紙廠1:目前剩%v噸木頭可用，不夠，休息\n", beforeWoodNum)
+			//阿 木頭不夠
+			fmt.Printf("%c[0;40;%dm紙廠%v:目前剩%v噸木頭可用，不夠，休息\n%c[0m", 0x1B, p.color, who, beforeWoodNum, 0x1B)
 		}
-
-		//解鎖
-		mutex.Unlock()
 
 		time_s := time.Duration(1) * time.Second
 		time.Sleep(time_s)
@@ -124,55 +117,24 @@ func paperFunc1(p paperrrr, allWood *float64, allPaper *int, allPrint *int, wg *
 	wg.Done()
 }
 
-func paperFunc2(p paperrrr, allWood *float64, allPaper *int, allPrint *int, wg *sync.WaitGroup, mutex sync.Mutex) {
+func printerFunc(p printerrrrr, allPaper *int, allPrint *int) {
 
-	//印好60000紙之前都給我好好造紙
+	//印好60000紙之前都給我好好去印啦印啦印啦印啦印啦印啦
 	for i := *allPrint; i < 60000; i = *allPrint {
-		//工作前的數量
-		beforeWoodNum := *allWood
-		//鎖
-		mutex.Lock()
-
-		//木頭有超過可拿取量 就去生產紙
-		if *allWood >= p.useWooderKg {
-			*allPaper = *allPaper + p.producePaper //紙+
-			*allWood = *allWood - p.useWooderKg    //木頭-
-			fmt.Printf("~~~紙廠2:目前有%v噸木頭用，累計產了%v張紙，剩下%v噸木頭\n", beforeWoodNum, *allPaper, *allWood)
-		} else {
-			fmt.Printf("~~~紙廠2:目前剩%v噸木頭可用，不夠，休息\n", beforeWoodNum)
-		}
-
-		//解鎖
-		mutex.Unlock()
-
-		time_s := time.Duration(1) * time.Second
-		time.Sleep(time_s)
-	}
-
-	//工作完成 回報WaitGroup -1
-	wg.Done()
-}
-
-func printerFunc(p printerrrrr, allPaper *int, allPrint *int, wg *sync.WaitGroup) {
-
-	//印好60000紙之前都給我好好印
-	for i := *allPrint; i < 60000; i = *allPrint {
-		//工作前的數量
+		//工作前的 紙張數量
 		beforePaperNum := *allPaper
-		//木頭有超過可拿取量 就去生產紙
+		//木頭有超過可拿取量 就去印印印印印紙
 		if *allPaper > p.printNum {
 			*allPrint = *allPrint + p.printNum //印刷紙+
 			*allPaper = *allPaper - p.printNum //紙-
-			fmt.Printf("+++印刷廠:目前有%v張紙可用，累計印了%v張紙，剩下%v張紙\n", beforePaperNum, *allPrint, *allPaper)
+			fmt.Printf("%c[0;40;%dm印刷廠:目前有%v張紙可用，累計印了%v張紙，剩下%v張紙\n%c[0m", 0x1B, p.color, beforePaperNum, *allPrint, *allPaper, 0x1B)
 		} else {
-			fmt.Printf("+++印刷廠:目前只剩有%v張紙，紙不夠，休息\n", beforePaperNum)
+			//阿 紙不夠
+			fmt.Printf("%c[0;40;%dm印刷廠:目前只剩有%v張紙，紙不夠，休息\n%c[0m", 0x1B, p.color, beforePaperNum, 0x1B)
 		}
 
 		time_s := time.Duration(1) * time.Second
 		time.Sleep(time_s)
 	}
-
-	//工作完成 回報WaitGroup -1
-	wg.Done()
 
 }

944cc4ce7bb24ebf03062734056088f3919c93f6
tseng duke <bearbearbear10675@gmail.com>
2018-08-23 08:52:34 +0000

    10

--- HW10/10.go
+++ HW10/10.go
@@ -14,28 +14,71 @@ package main
 
 import (
 	"fmt"
-	// "github.com/robfig/cron"
-	"io/ioutil"
-	// "log"
+	"github.com/robfig/cron"
+	// "io/ioutil"
 	"net/http"
+	"time"
 )
 
 func main() {
-	api()
-	// i := 0
-	// c := cron.New()
-	// spec := "*/1 * * * * *"
-	// c.AddFunc(spec, func() {
-	// 	i++
-	// 	log.Println("execute per second", i)
-	// })
-	// c.Start()
-	// select {}
+	round := 0 //回合
+	var timeSlice []time.Duration
+	var max time.Duration
+	var min time.Duration
+	c := cron.New()
+	spec := "*/1 * * * * *"
+	c.AddFunc(spec, func() {
+		round++
+		start := time.Now()
+		go apiRoutine(round)
+		t := time.Now()
+		elapsed := t.Sub(start)
+		timeSlice = append(timeSlice, elapsed)
+		if round == 1 {
+			min = elapsed
+		}
+		//all
+		fmt.Println("目前有", timeSlice)
+
+		//max min
+		if elapsed > max {
+			max = elapsed
+		}
+		if elapsed < min {
+			min = elapsed
+		}
+		fmt.Println("單一最大花費時間", max)
+		fmt.Println("單一最小花費時間", min)
+
+		//sum
+		var totalx time.Duration
+		var sumNum time.Duration
+		for _, valuex := range timeSlice {
+			sumNum++
+			totalx += valuex
+		}
+		fmt.Println("總時間", totalx)
+
+		//avg
+		fmt.Println("平均", totalx/sumNum)
+
+		fmt.Println("-----")
+
+	})
+	c.Start()
+	select {}
+}
+
+func apiRoutine(round int) {
+	for i := 0; i < round; i++ {
+		api()
+	}
 }
 
 func api() {
+	// resp, _ := http.Get("http://weather.json.tw/api")
 	resp, _ := http.Get("http://bb.dev.d2/api/domain/32/levels")
 	defer resp.Body.Close()
-	body, _ := ioutil.ReadAll(resp.Body)
-	fmt.Println(string(body))
+	// body, _ := ioutil.ReadAll(resp.Body)
+	// return body
 }
